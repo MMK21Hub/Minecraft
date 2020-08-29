ca7cceae2cd52aac7369ad7815d8fe83feecc645
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2020-08-29 13:36:39 +0100

    [Edits from last time]

--- Useful/OptionsBGgen/index.js
+++ Useful/OptionsBGgen/index.js
@@ -2,6 +2,7 @@ function loadFile(filePath) { // https://stackoverflow.com/a/41133213/11519302
     var result = null;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", filePath, false);
+    xmlhttp.setRequestHeader('x-requested-with', 'XMLHttpRequest');
     xmlhttp.send();
     if (xmlhttp.status == 200) {
         result = xmlhttp.responseText;
@@ -9,9 +10,12 @@ function loadFile(filePath) { // https://stackoverflow.com/a/41133213/11519302
     return result;
 }
 
-remoteControlRaw = loadFile("https://gist.githubusercontent.com/MMK21Hub/bbd7afbc74eb582c1a9d78b031b24f94/raw/e2b3ee285f9f1c27d6899cc5fcb163667808fce3/remoteControl.json")
-window.remoteControl = JSON.parse(remoteControlRaw)
+commitsList = JSON.parse(loadFile("https://api.github.com/gists/bbd7afbc74eb582c1a9d78b031b24f94/commits"))
+latestCommit = loadFile(commitsList[0].url)
+remoteControl = latestCommit.files
 console.log(remoteControl)
-if (remoteControl.run == true) {
+/*if (remoteControl.run == true) {
     console.log("run")
-}
+} else{
+    document.getElementById("body").innerHTML("<p>Hello</p>")
+}*/
