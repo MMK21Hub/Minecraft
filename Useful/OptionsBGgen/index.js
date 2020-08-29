8a479305544c5f2cd338f504783a70838efce0ce
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2020-08-29 14:30:58 +0100

    Add selection

--- Useful/OptionsBGgen/index.js
+++ Useful/OptionsBGgen/index.js
@@ -10,17 +10,22 @@ function loadFile(filePath) { // https://stackoverflow.com/a/41133213/11519302
     return result;
 }
 
-commitsList = JSON.parse(loadFile("https://api.github.com/gists/bbd7afbc74eb582c1a9d78b031b24f94/commits"))
-latestCommit = JSON.parse(loadFile(commitsList[0].url))
-remoteControl = latestCommit.files["remoteControl.json"].content
-remoteControl = JSON.parse(remoteControl)
-downMsg = remoteControl.downMsg
+commitsList = JSON.parse(loadFile("https://api.github.com/gists/bbd7afbc74eb582c1a9d78b031b24f94/commits"));
+latestCommit = JSON.parse(loadFile(commitsList[0].url));
+remoteControl = latestCommit.files["remoteControl.json"].content;
+remoteControl = JSON.parse(remoteControl);
+downMsg = remoteControl.downMsg;
 if (downMsg == undefined) {
-    downMsg = ""
+    downMsg = "";
 }
-console.log(downMsg)
 if (remoteControl.run == true) {
-    console.log("Remote Control OK!")
+    console.log("Remote Control OK!");
 } else{
     document.getElementById("body").innerHTML = "<p>The Options Background Generator has been disabled remotely. Check back later?</p><p><small><em>"+downMsg+"</em></small></p>";
+}
+
+function readValue() {
+    var blocks = document.getElementById("blocks");
+    var selectedBlock = blocks.value;
+    console.log(selectedBlock)
 }
