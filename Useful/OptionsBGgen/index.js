c83cdf47cb3aa18d14edb8b855d96227e1d0de7d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2020-09-26 09:48:54 +0100

    FINALLY got stuff working

--- Useful/OptionsBGgen/index.js
+++ Useful/OptionsBGgen/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 function loadFile(filePath) { // https://stackoverflow.com/a/41133213/11519302
     var result = null;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", filePath, false);
-    xmlhttp.setRequestHeader('x-requested-with', 'XMLHttpRequest');
+    xmlhttp.setRequestHeader("x-requested-with", "XMLHttpRequest");
     xmlhttp.send();
     if (xmlhttp.status == 200) {
         result = xmlhttp.responseText;
@@ -10,16 +12,25 @@ function loadFile(filePath) { // https://stackoverflow.com/a/41133213/11519302
     return result;
 }
 
+// Get remote control
 commitsList = JSON.parse(loadFile("https://api.github.com/gists/bbd7afbc74eb582c1a9d78b031b24f94/commits"));
 latestCommit = JSON.parse(loadFile(commitsList[0].url));
 remoteControl = latestCommit.files["remoteControl.json"].content;
 remoteControl = JSON.parse(remoteControl);
 downMsg = remoteControl.downMsg;
+
+// Act on Remote Control
 if (downMsg == undefined) {
     downMsg = "";
 }
 if (remoteControl.run == true) {
     console.log("Remote Control OK!");
+    // Only proceed if site is not disabled
+    
+    // Get version manifest
+    versionManifest = JSON.parse(loadFile("https://cors-anywhere.herokuapp.com/https://launchermeta.mojang.com/mc/game/version_manifest.json"));
+    console.log(versionManifest.latest.snapshot);
+
 } else{
     document.getElementById("body").innerHTML = "<p>The Options Background Generator has been disabled remotely. Check back later?</p><p><small><em>"+downMsg+"</em></small></p>";
 }
@@ -27,5 +38,5 @@ if (remoteControl.run == true) {
 function readValue() {
     var blocks = document.getElementById("blocks");
     var selectedBlock = blocks.value;
-    console.log(selectedBlock)
+    console.log(selectedBlock);
 }
